89e02c24df06c6946cb8d1289b242f2ccbdf1f35
osasere77 <osasereaibangbee777@gmail.com>
2026-03-13 21:47:04 +0100

    wk2  complete assignment

---
+++ temples.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+navigation.classList.toggle("open");
+
+if(menuButton.textContent === "☰"){
+menuButton.textContent = "X";
+}else{
+menuButton.textContent = "☰";
+}
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent = document.lastModified;
